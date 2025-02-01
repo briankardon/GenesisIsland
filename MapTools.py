19a908c59982de48000fe11a2b7e11f1eb8185bd
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2025-02-01 12:18:43 -0500

    Fixed output map shape

--- MapTools.py
+++ MapTools.py
@@ -8,6 +8,11 @@ def make_diamond_mask(power):
     mask = np.mod(np.reshape(range(side*side), (side, side))+1, 2).astype('bool')
     return mask
 
+def center_crop(array, new_shape):
+    start = (np.array(array.shape) - new_shape) // 2
+    end = start + new_shape
+    return array[start[0]:end[0], start[1]:end[1]]
+
 def make_map(size, blur_size=3, min_value=0, max_value=1, integer=False):
     # Generate a random heightmap using the diamond-square algorithm
     rng = np.random.default_rng()
@@ -45,13 +50,14 @@ def make_map(size, blur_size=3, min_value=0, max_value=1, integer=False):
     new_range = max_value-min_value
     map = gaussian_filter(map, (blur_size, blur_size), mode='wrap')
     map = (map - map_min) * (new_range / mapRange) + min_value
+    map = center_crop(map, size)
     if integer:
         return map.astype('uint8')
     else:
         return map
 
 if __name__ == '__main__':
-    map = make_map((1000, 1000), blur_size=12, max_value=255, integer=True)
+    map = make_map((800, 600), blur_size=12, max_value=255, integer=True)
     map3 = np.zeros(map.shape+(3,), dtype='uint8')
     map3[:, :, 0] = (map < 150) * map
     map3[:, :, 1] = (map >= 150) * map
