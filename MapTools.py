c012f68922cd79c98e3ff7230e75d3e199fb5956
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2025-02-01 12:01:30 -0500

    Completed MapTools.make_map

--- MapTools.py
+++ MapTools.py
@@ -8,7 +8,7 @@ def make_diamond_mask(power):
     mask = np.mod(np.reshape(range(side*side), (side, side))+1, 2).astype('bool')
     return mask
 
-def make_map(size):
+def make_map(size, blur_size=3, min_value=0, max_value=1, integer=False):
     # Generate a random heightmap using the diamond-square algorithm
     rng = np.random.default_rng()
 
@@ -39,15 +39,22 @@ def make_map(size):
         mask = make_diamond_mask(subpow+1)
         averages[mask] = 0
         map[::halfstep, ::halfstep] += averages
-    mapMax = map.max()
-    mapMin = map.min()
-    mapRange = mapMax-mapMin
-    map = (map - mapMin) / mapRange
-    map = gaussian_filter(map, (3, 3), mode='wrap')
-    return map
+    map_max = map.max()
+    map_min = map.min()
+    mapRange = map_max-map_min
+    new_range = max_value-min_value
+    map = gaussian_filter(map, (blur_size, blur_size), mode='wrap')
+    map = (map - map_min) * (new_range / mapRange) + min_value
+    if integer:
+        return map.astype('uint8')
+    else:
+        return map
 
 if __name__ == '__main__':
-    map = make_map((1000, 1000))
-    cv2.imshow('map', np.kron(map,np.ones((8, 8))))
+    map = make_map((1000, 1000), blur_size=12, max_value=255, integer=True)
+    map3 = np.zeros(map.shape+(3,), dtype='uint8')
+    map3[:, :, 0] = (map < 150) * map
+    map3[:, :, 1] = (map >= 150) * map
+    cv2.imshow('map', map3)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
