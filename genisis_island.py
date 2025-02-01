9a97cfa01e9a69b410a5be333891c4dcbbe2344f
silas <silasrileykardon@gmail.com>
2025-02-01 11:20:17 -0500

    trying to make pathfinder, does  not work.

--- genisis_island.py
+++ genisis_island.py
@@ -22,19 +22,101 @@ def goodrand(which,*args):
         return do
 screen=pygame.display.set_mode((width, height))
 board=[]
+first=1
 for bleh in range(72):
     board.append([])
     for bluh in range(36):
-        board[bleh].append((randint(150,230),goodrand('choice',('grass',4),('mountains',1),('trees',2),('sheep',1),('water',2))))
+        dograss=4
+        domountains=1
+        dotrees=2
+        dosheep=1
+        dowater=2
+        ##try:
+        ##    if board[bleh][bluh-1]=='grass' or board[bleh][bluh+1]=='grass' or board[bleh+1][bluh]=='grass' or board[bleh-1][bluh]=='grass':
+        ##        dograss=(5+999999999999999999999999999999999999999999999999999*98999999999999999999999999*999999999999999999999999)
+        ##    if board[bleh][bluh-1]=='mountains' or board[bleh][bluh+1]=='mountains' or board[bleh+1][bluh]=='mountains' or board[bleh-1][bluh]=='mountains':
+        ##        domountains=(2+999999999999999999999999999999999999999999999999999*98999999999999999999999999*999999999999999999999999)
+        ##    if board[bleh][bluh-1]=='trees' or board[bleh][bluh+1]=='trees' or board[bleh+1][bluh]=='trees' or board[bleh-1][bluh]=='trees':
+        ##        dotrees=(3+999999999999999999999999999999999999999999999999999*98999999999999999999999999*999999999999999999999999)
+        ##    if board[bleh][bluh-1]=='sheep' or board[bleh][bluh+1]=='sheep' or board[bleh+1][bluh]=='sheep' or board[bleh-1][bluh]=='sheep':
+        ##        dosheep=(2+999999999999999999999999999999999999999999999999999*98999999999999999999999999*999999999999999999999999)
+        ##    if board[bleh][bluh-1]=='water' or board[bleh][bluh+1]=='water' or board[bleh+1][bluh]=='water' or board[bleh-1][bluh]=='water':
+        ##        dowater=(3+999999999999999999999999999999999999999999999999999*98999999999999999999999999*999999999999999999999999)
+        ##except IndexError:
+        ##    pass
+        board[bleh].append((randint(150,230),goodrand('choice',('grass',dograss),('mountains',domountains),('trees',dotrees),('sheep',dosheep),('water',dowater))))
 yx=randint(1,72)
 yy=randint(1,36)
 cool=0
+class Pathfinder:
+    def __init__(self,board,x,y,mex,mey):
+        self.open=[]
+        self.path=[]
+        self.board=board
+        self.endx=x
+        self.endy=y
+        self.x=mex
+        self.y=mey
+    def makepath(self):
+        move=self.path
+        if self.x>self.endx:
+            try:
+                if self.board[self.x-1][self.x]=='grass':
+                    self.path.append([self.x-1,self.y])
+            except IndexError:
+                pass
+        else:
+            try:
+                if self.board[self.x+1][self.x]=='grass':
+                    self.path.append([self.x+1,self.y])
+            except IndexError:
+                pass
+        if self.y>self.endy:
+            try:
+                if self.board[self.y-1][self.y]=='grass':
+                    self.path.append([self.x,self.y-1])
+            except IndexError:
+                pass
+        else:
+            try:
+                if self.board[self.y+1][self.y]=='grass':
+                    self.path.append([self.x,self.y+1])
+            except IndexError:
+                pass
+    def move(self):
+        if self.x>self.endx:
+            for bleh in range(len(self.path)):
+                for bluh in range(len(self.path[bleh])):
+                    if self.path[bleh][bluh]==self.x-1:
+                        self.x-=1
+        else:
+            for bleh in range(len(self.path)):
+                for bluh in range(len(self.path[bleh])):
+                    if self.path[bleh][bluh]==self.x+1:
+                        self.x+=1
+        if self.y>self.endy:
+            for bleh in range(len(self.path)):
+                for bluh in range(len(self.path[bleh])):
+                    if self.path[bleh][bluh]==self.y-1:
+                        self.y-=1
+        else:
+            for bleh in range(len(self.path)):
+                for bluh in range(len(self.path[bleh])):
+                    if self.path[bleh][bluh]==self.y+1:
+                        self.y+=1
+x,y=pygame.mouse.get_pos()
 while True:
+    try:
+        dude.makepath()
+        dude.move()
+    except NameError:
+        pass
     screen.fill('blue')
     pygame.mouse.set_visible(False)
     x,y=pygame.mouse.get_pos()
     for event in pygame.event.get():
-        pass
+        if event.type==pygame.MOUSEBUTTONDOWN:
+            dude=Pathfinder(board,x,y,yx,yy)
     key=pygame.key.get_pressed()
     if key[pygame.K_x]:
         pygame.quit()
@@ -56,5 +138,9 @@ while True:
     pygame.draw.circle(screen,[255,100,100],(x,y),5)
     pygame.draw.circle(screen,'red',(yx*20-10,yy*20-10),10)
     pygame.draw.line(screen,'white',(yx*20-10,yy*20-10),(x,y),2)
+    try:
+        pygame.draw.circle(screen,'red',(dude.x*20,dude.y*20),10)
+    except NameError:
+        pass
     pygame.display.flip()
     Clock.tick(60)
